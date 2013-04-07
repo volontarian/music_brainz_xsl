7a0eea3bad4f241b9a0fb8e04e172a29ba4f5787
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-04-07 20:06:43 +0200

    Get refs like relations also through zeroOrMore. Get some list accessors as primitive type (string). Adds new accessor target_type from relation-list attributes. Include SearchResultMapper.

--- mappers/ruby/relation.rb
+++ mappers/ruby/relation.rb
@@ -1,7 +1,8 @@
 module MusicBrainz
   class Relation
-    include ROXML
-        
+include ROXML, ::MusicBrainzXsl::SearchResultMapper
+
+attr_accessor :target_type
     xml_accessor :type, from: '@type'
     xml_accessor :type_id, from: '@type-id'
 
@@ -11,6 +12,12 @@ module MusicBrainz
     xml_accessor :end, from: 'end'
     xml_accessor :ended, from: 'ended'
 
-    xml_accessor :attributes, from: 'attribute-list/attribute', as: [Attribute]
+    xml_accessor :attributes, from: 'attribute-list/attribute', as: []
+    xml_accessor :artist, from: 'artist', as: Artist
+    xml_accessor :release, from: 'release', as: Release
+    xml_accessor :release_group, from: 'release-group', as: ReleaseGroup
+    xml_accessor :recording, from: 'recording', as: Recording
+    xml_accessor :label, from: 'label', as: Label
+    xml_accessor :work, from: 'work', as: Work
   end
 end
