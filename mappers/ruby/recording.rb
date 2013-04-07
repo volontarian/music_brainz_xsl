d9aad10457717fa77a0e7e2590ef2f03039e2f5b
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-04-07 20:06:27 +0200

    Improves artists alias NameCredit accessor. Get refs like relations also through zeroOrMore. Include SearchResultMapper.

--- mappers/ruby/recording.rb
+++ mappers/ruby/recording.rb
@@ -1,7 +1,6 @@
 module MusicBrainz
   class Recording
-    include ROXML
-        
+include ROXML, ::MusicBrainzXsl::SearchResultMapper
     xml_accessor :id, from: '@id'
 
     xml_accessor :title, from: 'title'
@@ -10,10 +9,11 @@ module MusicBrainz
     xml_accessor :user_rating, from: 'user-rating'
 
     xml_accessor :annotation, from: 'annotation', as: Annotation
-    xml_accessor :artist_credit, from: 'artist-credit/name-credit', as: [NameCredit]
+    xml_accessor :artists, from: 'artist-credit/name-credit/artist', as: [::MusicBrainz::NameCredit]
     xml_accessor :releases, from: 'release-list/release', as: [Release]
     xml_accessor :puids, from: 'puid-list/puid', as: [Puid]
     xml_accessor :isrcs, from: 'isrc-list/isrc', as: [Isrc]
+    xml_accessor :relations, from: 'relation-list/relation', as: [Relation]
     xml_accessor :tags, from: 'tag-list/tag', as: [Tag]
     xml_accessor :user_tags, from: 'user-tag-list/user-tag', as: [UserTag]
     xml_accessor :rating, from: 'rating', as: Rating
