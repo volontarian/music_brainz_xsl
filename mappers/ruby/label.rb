c8e5ecd08785893ec8462b5d9a2e84fbc743d02e
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-04-07 20:05:57 +0200

    Get refs like relations also through zeroOrMore. Get some list accessors as primitive type (string). Get some accessors through parent. Improves pluralization of alias. Include SearchResultMapper.

--- mappers/ruby/label.rb
+++ mappers/ruby/label.rb
@@ -1,7 +1,6 @@
 module MusicBrainz
   class Label
-    include ROXML
-        
+include ROXML, ::MusicBrainzXsl::SearchResultMapper
     xml_accessor :id, from: '@id'
     xml_accessor :type, from: '@type'
 
@@ -11,14 +10,15 @@ module MusicBrainz
     xml_accessor :ipi, from: 'ipi'
     xml_accessor :disambiguation, from: 'disambiguation'
     xml_accessor :country, from: 'country'
-    xml_accessor :begin, from: 'begin'
-    xml_accessor :end, from: 'end'
+    xml_accessor :begin, from: 'life-span/begin'
+    xml_accessor :end, from: 'life-span/end'
     xml_accessor :user_rating, from: 'user-rating'
 
-    xml_accessor :ipis, from: 'ipi-list/ipi', as: [Ipi]
+    xml_accessor :ipis, from: 'ipi-list/ipi', as: []
     xml_accessor :annotation, from: 'annotation', as: Annotation
-    xml_accessor :aliass, from: 'alias-list/alias', as: [Alias]
+    xml_accessor :aliases, from: 'alias-list/alias', as: [Alias]
     xml_accessor :releases, from: 'release-list/release', as: [Release]
+    xml_accessor :relations, from: 'relation-list/relation', as: [Relation]
     xml_accessor :tags, from: 'tag-list/tag', as: [Tag]
     xml_accessor :user_tags, from: 'user-tag-list/user-tag', as: [UserTag]
     xml_accessor :rating, from: 'rating', as: Rating
