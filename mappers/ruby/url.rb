6ea125bcd8fcf08fcaca3a6cb097da069244d112
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-04-07 20:07:37 +0200

    Get refs like relations also through zeroOrMore. Include SearchResultMapper.

--- mappers/ruby/url.rb
+++ mappers/ruby/url.rb
@@ -1,9 +1,10 @@
 module MusicBrainz
   class Url
-    include ROXML
-        
+include ROXML, ::MusicBrainzXsl::SearchResultMapper
     xml_accessor :id, from: '@id'
 
     xml_accessor :resource, from: 'resource'
+
+    xml_accessor :relations, from: 'relation-list/relation', as: [Relation]
   end
 end
