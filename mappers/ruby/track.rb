707b949fa19c231082eee1dc8f62020e37710656
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-04-07 20:07:22 +0200

    Improves artists alias NameCredit accessor. Include SearchResultMapper.

--- mappers/ruby/track.rb
+++ mappers/ruby/track.rb
@@ -1,13 +1,12 @@
 module MusicBrainz
   class Track
-    include ROXML
-        
+include ROXML, ::MusicBrainzXsl::SearchResultMapper
     xml_accessor :position, from: 'position'
     xml_accessor :number, from: 'number'
     xml_accessor :title, from: 'title'
     xml_accessor :length, from: 'length'
 
-    xml_accessor :artist_credit, from: 'artist-credit/name-credit', as: [NameCredit]
+    xml_accessor :artists, from: 'artist-credit/name-credit/artist', as: [::MusicBrainz::NameCredit]
     xml_accessor :recording, from: 'recording', as: Recording
   end
 end
